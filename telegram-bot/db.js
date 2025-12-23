8f0b1594a93521a19c8910ed6302937b5ee5eda4
Nebiyu <nebiyutsegaye213@gmail.com>
2025-12-23 07:29:46 -0900

    Add SSL config for production DB connection

--- telegram-bot/db.js
+++ telegram-bot/db.js
@@ -4,6 +4,9 @@ const { Pool } = require('pg');
 // based on the configuration in render.yaml
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? {
+    rejectUnauthorized: false
+  } : false,
 });
 
 module.exports = {
